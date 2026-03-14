39227790bde89888cd4487c112e1292b1bd27506
Mayank <mayankkanti2325@gmail.com>
2026-03-14 22:41:01 +0530

    assignment1 - Q7

--- assignment1/college_project/studentApp/models.py
+++ assignment1/college_project/studentApp/models.py
@@ -3,6 +3,11 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
+    roll_number = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
