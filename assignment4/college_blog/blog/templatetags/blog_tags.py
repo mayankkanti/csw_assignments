21d1eb670a646d5d339403590b0cb4baef109b45
Mayank <mayankkanti2325@gmail.com>
2026-04-30 10:01:01 +0530

    feat(assignment4): Q5 - custom template tags (total, latest, most commented)

---
+++ assignment4/college_blog/blog/templatetags/blog_tags.py
@@ -0,0 +1,23 @@
+from django import template
+from django.template.loader import render_to_string
+from ..models import Post
+from django.db.models import Count
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def latest_posts(count=5):
+    posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': posts}
+
+
+@register.inclusion_tag('blog/post/most_commented.html')
+def most_commented(count=5):
+    posts = Post.published.annotate(comment_count=Count('comments')).order_by('-comment_count')[:count]
+    return {'most_commented_posts': posts}
